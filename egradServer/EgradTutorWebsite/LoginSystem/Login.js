cac6ae3facd6852cfb136cc579c433921720ab13
VeenaRagi <163105402+VeenaRagi@users.noreply.github.com>
2024-07-09 11:21:54 +0530

    Completed encryption decryption

--- egradServer/EgradTutorWebsite/LoginSystem/Login.js
+++ egradServer/EgradTutorWebsite/LoginSystem/Login.js
@@ -4,12 +4,20 @@ const db = require("../../DataBase/db2");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const CryptoJS = require('crypto-js');
+const cors=require('cors')
+router.use(cors());
+router.use(cors({
+  origin: 'http://localhost:3000', // Update to your client URL
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type,Authorization'
+}));
+const secretKey=process.env.LOCAL_STORAGE_SECRET_KEY_FOR_USER_ID;
+router.use(express.json());
+router.options('*', cors()); // Enable preflight requests
 
-// Login route
 router.post('/login', async (req, res) => {
     const { email, password } = req.body;
-    // console.log('Login request received:', { email, password });
-  
     try {
       // Fetch user from the database by email
       const sql = 'SELECT user_Id,email,password,role FROM log WHERE email = ?';
@@ -27,49 +35,18 @@ router.post('/login', async (req, res) => {
       const isMatch = await bcrypt.compare(password, user.password);
   
       if (!isMatch) {
-        // console.log('Password mismatch for user:', user);
         return res.status(400).json({ message: 'Invalid email or password' });
       }
-  
       // Generate JWT token
-      const accesToken = jwt.sign({ user_Id: user.user_Id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+      const accessToken = jwt.sign({ user_Id: user.user_Id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
       console.log('Token generated for user:', user);
       const refreshToken = jwt.sign({ user_Id: user.user_Id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '7d' });
-      console.log("refreshToken:",refreshToken,"accessToken: ",accesToken )
-      res.json({user_Id:user.user_Id,refreshToken, accesToken, role: user.role });
+      console.log("refreshToken:",refreshToken,"accessToken: ",accessToken )
+      console.log("sending detail are",user.user_Id,refreshToken,accessToken)
+      res.json({user_Id: user.user_Id,refreshToken, accessToken, role: user.role });
     } catch (error) {
       console.error('Error during login:', error);
       res.status(500).json({ message: 'Server error' });
     }
   });
-
-
-//   router.post('/refresh-token', async (req, res) => {
-//     const { refreshToken } = req.body;
-
-//     if (!refreshToken) {
-//         return res.status(401).json({ message: 'Refresh token is required' });
-//     }
-
-//     try {
-//         jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-//             if (err) {
-//                 return res.status(403).json({ message: 'Invalid refresh token' });
-//             }
-
-//             // Generate a new access token
-//             const newAccessToken = jwt.sign({ user_Id: user.user_Id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
-            
-//             res.json({ accessToken: newAccessToken });
-//         });
-//     } catch (error) {
-//         console.error('Error during token refresh:', error);
-//         res.status(500).json({ message: 'Server error' });
-//     }
-// });
-
-const crypto = require('crypto');
-const secretKey = crypto.randomBytes(32).toString('hex');
-console.log('Generated Secret Key:', secretKey);
-
 module.exports = router;
--- egradfrontend/src/App.js
+++ egradfrontend/src/App.js
@@ -1,40 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './assets/actions/store';
-import WebSiteLandingPage from './EgradTutorFrontEnd/EgradTutorWebsit/WebsiteLandingPage/WebSiteLandingPage';
-import BranchHomePage from './EgradTutorFrontEnd/EgradTutorWebsit/BranchHomePage/BranchHomePage';
-import ExamHomePage from './EgradTutorFrontEnd/EgradTutorWebsit/ExamHomePage/ExamHomePage';
-import { ThemeProvider } from './ThemesFolder/ThemeContext/Context';
-import AboutUs from './EgradTutorFrontEnd/EgradTutorWebsit/WebsiteSubPages/AboutUsPage/AboutUs';
-import ContactUs from './EgradTutorFrontEnd/EgradTutorWebsit/WebsiteSubPages/ContactUs/ContactUs';
-import Login from './Login/Login';
-import AdminLogin from './Login/AdminLogin';
-import Register from './Login/Register';
-import UgadminHome from './Login/UgadminHome';
-import LinkPage from './EgradTutorFrontEnd/EgradTutorWebsit/Footer/LinkPage';
-import FAQ from './EgradTutorFrontEnd/EgradTutorWebsit/WebsiteSubPages/FAQPage/FAQ';
-import CoursePage from './EgradTutorFrontEnd/EgradTutorWebsit/CoursePages/CoursePage';
-import UserLogin from './Login/UserLogin';
-import StudentRegistrationPage from './EgradTutorFrontEnd/EgradTutorWebsit/StudentDashbord/StudentRegistationPage';
-import PasswordChangeForm from './EgradTutorFrontEnd/EgradTutorWebsit/StudentDashbord/PasswordChangeForm';
-import SuperAdminLogin from './Login/SuperAdminLogin';
-import UserDashboard from './EgradTutorFrontEnd/EgradTutorWebsit/StudentDashbord/UserDashboard ';
-import PrivateRoute from './Login/PrivateRoute';
-import ForgotPassword from './Login/ForgotPassword';
-import Maintenance1 from './EgradTutorFrontEnd/MaintenanceMode/Maintenance1.js';
-import StudentRegistrationPageBuynow from './EgradTutorFrontEnd/EgradTutorWebsit/StudentDashbord/StudentRegistrationPageBuynow';
-import { TIAuthProvider } from './TechInfoContext/AuthContext.js';
-import axios from './api/axios.js';
-import BASE_URL from './apiConfig.js';
-import NotFound from './NotFound.jsx';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./assets/actions/store";
+import WebSiteLandingPage from "./EgradTutorFrontEnd/EgradTutorWebsit/WebsiteLandingPage/WebSiteLandingPage";
+import BranchHomePage from "./EgradTutorFrontEnd/EgradTutorWebsit/BranchHomePage/BranchHomePage";
+import ExamHomePage from "./EgradTutorFrontEnd/EgradTutorWebsit/ExamHomePage/ExamHomePage";
+import { ThemeProvider } from "./ThemesFolder/ThemeContext/Context";
+import AboutUs from "./EgradTutorFrontEnd/EgradTutorWebsit/WebsiteSubPages/AboutUsPage/AboutUs";
+import ContactUs from "./EgradTutorFrontEnd/EgradTutorWebsit/WebsiteSubPages/ContactUs/ContactUs";
+import Login from "./Login/Login";
+import AdminLogin from "./Login/AdminLogin";
+import Register from "./Login/Register";
+import UgadminHome from "./Login/UgadminHome";
+import LinkPage from "./EgradTutorFrontEnd/EgradTutorWebsit/Footer/LinkPage";
+import FAQ from "./EgradTutorFrontEnd/EgradTutorWebsit/WebsiteSubPages/FAQPage/FAQ";
+import CoursePage from "./EgradTutorFrontEnd/EgradTutorWebsit/CoursePages/CoursePage";
+import UserLogin from "./Login/UserLogin";
+import StudentRegistrationPage from "./EgradTutorFrontEnd/EgradTutorWebsit/StudentDashbord/StudentRegistationPage";
+import PasswordChangeForm from "./EgradTutorFrontEnd/EgradTutorWebsit/StudentDashbord/PasswordChangeForm";
+import SuperAdminLogin from "./Login/SuperAdminLogin";
+import UserDashboard from "./EgradTutorFrontEnd/EgradTutorWebsit/StudentDashbord/UserDashboard ";
+import PrivateRoute from "./Login/PrivateRoute";
+import ForgotPassword from "./Login/ForgotPassword";
+import Maintenance1 from "./EgradTutorFrontEnd/MaintenanceMode/Maintenance1.js";
+import StudentRegistrationPageBuynow from "./EgradTutorFrontEnd/EgradTutorWebsit/StudentDashbord/StudentRegistrationPageBuynow";
+import { TIAuthProvider } from "./TechInfoContext/AuthContext.js";
+import axios from "./api/axios.js";
+import BASE_URL from "./apiConfig.js";
+import NotFound from "./NotFound.jsx";
+import SDAfterLogin from "./EgradTutorFrontEnd/EgradTutorWebsit/StudentDashbord/SDAfterLogin.jsx";
 function App() {
   const [isEditMode, setIsEditMode] = useState(false);
   const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
-    const userRole = localStorage.getItem('userRole');
-    if (userRole === 'admin') {
+    const userRole = localStorage.getItem("userRole");
+    if (userRole === "admin") {
       setIsAdmin(true);
     }
   }, []);
@@ -66,20 +67,22 @@ function App() {
       <div>
         {isAdmin && (
           <button onClick={toggleEditMode}>
-            {isEditMode ? 'Disable Edit' : 'Enable Edit'}
+            {isEditMode ? "Disable Edit" : "Enable Edit"}
           </button>
         )}
 
         {serverError ? (
           <div>
-            <NotFound/>
+            <NotFound />
           </div>
         ) : (
           <div>
-
             <Router>
               <Routes>
-                <Route path="/" element={<WebSiteLandingPage isEditMode={isEditMode} />} />
+                <Route
+                  path="/"
+                  element={<WebSiteLandingPage isEditMode={isEditMode} />}
+                />
               </Routes>
             </Router>
           </div>
@@ -92,11 +95,26 @@ function App() {
               <Route path="/Register" element={<Register />} />
               <Route path="/UgadminHome" element={<UgadminHome />} />
               {/* <Route path="/" element={<WebSiteLandingPage isEditMode={isEditMode} />} /> */}
-              <Route path="/BranchHomePage/:Branch_Id" element={<BranchHomePage isEditMode={isEditMode} />} />
-              <Route path="/ExamHomePage/:EntranceExams_Id" element={<ExamHomePage isEditMode={isEditMode} />} />
-              <Route path="/CoursePage/:Branch_Id/:Portale_Id" element={<CoursePage isEditMode={isEditMode} />} />
-              <Route path="/StudentRegistrationPageBuynow/:courseCreationId" element={<StudentRegistrationPageBuynow />} />
-              <Route path="/AboutUs" element={<AboutUs isEditMode={isEditMode} />} />
+              <Route
+                path="/BranchHomePage/:Branch_Id"
+                element={<BranchHomePage isEditMode={isEditMode} />}
+              />
+              <Route
+                path="/ExamHomePage/:EntranceExams_Id"
+                element={<ExamHomePage isEditMode={isEditMode} />}
+              />
+              <Route
+                path="/CoursePage/:Branch_Id/:Portale_Id"
+                element={<CoursePage isEditMode={isEditMode} />}
+              />
+              <Route
+                path="/StudentRegistrationPageBuynow/:courseCreationId"
+                element={<StudentRegistrationPageBuynow />}
+              />
+              <Route
+                path="/AboutUs"
+                element={<AboutUs isEditMode={isEditMode} />}
+              />
               <Route path="/ContactUs" element={<ContactUs />} />
               <Route path="/Faq" element={<FAQ />} />
               <Route path="/linkpage/:Link_Id" element={<LinkPage />} />
@@ -104,12 +122,19 @@ function App() {
               <Route path="/UserLogin" element={<UserLogin />} />
               <Route path="/forgot-password" element={<ForgotPassword />} />
               <Route path="/SuperAdminLogin" element={<SuperAdminLogin />} />
-              <Route path="/StudentRegistrationForm/:courseCreationId" element={<StudentRegistrationPage />} />
+              <Route
+                path="/StudentRegistrationForm/:courseCreationId"
+                element={<StudentRegistrationPage />}
+              />
               <Route path="/login/:userId" element={<PasswordChangeForm />} />
-              <Route path = "/Maintenance1" element = {<Maintenance1 />} />
-              <Route path="/user-dashboard/:userId" element={<PrivateRoute>
+              <Route path="/Maintenance1" element={<Maintenance1 />} />
+              {/* <Route path="/user-dashboard/:user_Id" element={<PrivateRoute>
                 <UserDashboard />
-              </PrivateRoute>} />
+              </PrivateRoute>} /> */}
+              <Route element={<PrivateRoute/>}>
+              <Route path="/user-dashboard/:user_Id" element={<UserDashboard />} />
+              <Route path="/testingUrl/:userIdTesting"element={<SDAfterLogin/>} />
+              </Route>
             </Routes>
           </Router>
         </Provider>
