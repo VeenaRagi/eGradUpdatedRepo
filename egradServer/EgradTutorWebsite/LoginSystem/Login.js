5bc1691c8133845bc8bd5db7a359a435e0385020
VeenaRagi <163105402+VeenaRagi@users.noreply.github.com>
2024-07-11 12:55:33 +0530

    ots student reg table instead of log

--- egradServer/EgradTutorWebsite/LoginSystem/Login.js
+++ egradServer/EgradTutorWebsite/LoginSystem/Login.js
@@ -64,7 +64,7 @@ router.post('/login', async (req, res) => {
             return res.status(400).json({ message: 'Invalid or missing user ID' });
         }
         // Retrieve user details from the database
-        const sql = 'SELECT * FROM log WHERE user_Id = ?';
+        const sql = 'SELECT * FROM otsstudentregistation WHERE user_Id = ?';
         const [users] = await db.query(sql, [userId]);
         if (users.length === 0) {
             return res.status(404).json({ message: 'User not found' });
