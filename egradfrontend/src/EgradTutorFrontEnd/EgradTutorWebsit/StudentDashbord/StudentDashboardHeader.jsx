1ffdb4b84d3296044a83cafe52435460c598852b
VeenaRagi <163105402+VeenaRagi@users.noreply.github.com>
2024-07-12 18:00:04 +0530

    logo added

--- egradfrontend/src/EgradTutorFrontEnd/EgradTutorWebsit/StudentDashbord/StudentDashboardHeader.jsx
+++ egradfrontend/src/EgradTutorFrontEnd/EgradTutorWebsit/StudentDashbord/StudentDashboardHeader.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import './Style/StudentDashboardHeader.css'
 import BASE_URL from '../../../apiConfig'
+import logo from '../../../styles/MicrosoftTeams-image (7).png'
 const StudentDashboardHeader = ({ usersData, decryptedUserIdState }) => {
     console.log("user daata from the student dashboard header", usersData)
+    
     return (
         <div>
             <div className='SDHParentContainer'>
                 <div className="SDHSubContainer">
                     <div className='SDHContentContainer'>
-                    Student DashboardHeader
+                    {/* Student DashboardHeader */}
+                    <div className='SDHeaderLogoContainer'> 
+                        <img src={logo} alt="no logo " />
+                    </div>
                     {usersData.users.map((user) => (
                         <>
-                            <div className='SDHeaderImgContainer'>
+                            <div className='SDHeaderImgContainer' style={{}}>
                                 <img style={{ width: "100%", borderRadius:"50%"}} src={`${BASE_URL}/uploads/studentinfoimeages/${user.UplodadPhto}`} alt={`no img${user.UplodadPhto}`} />
                             </div>
                             <div className='toBeDisplayedWhenHovered'>
