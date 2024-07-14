ecc15f4aed9a2da4d549c613ef3db4cfa53a9478
VeenaRagi <163105402+VeenaRagi@users.noreply.github.com>
2024-07-14 12:42:26 +0530

    getting username from context

--- egradfrontend/src/TechInfoContext/AuthContext.js
+++ egradfrontend/src/TechInfoContext/AuthContext.js
@@ -3,26 +3,44 @@ import { createContext, useContext, useEffect, useState } from "react";
 const TIAuthContext = createContext();
 
 const TIAuthProvider = ({ children }) => {
-  const [tiAuth, settiAuth] = useState({
+  const [tiAuth, settiAuth]= useState({
     user: "",
     token: "",
     isLoggedIn: false,
+    role: "",
+    userDecryptedId: null,
+    userData: null,  // Initialize userData as null
   });
-  useEffect(()=>{
-    const data=localStorage.getItem('tiAuth')
-    if(data){
-        const parseData=JSON.parse(data)
-        settiAuth({
-            ...tiAuth,
-            user:parseData.user,
-            token:parseData.token,
-            role:parseData.role,
-            // userDecryptedId:parseData.user_Id
-            userDecryptedId:parseData.decryptedId
-        })
+  // useEffect(()=>{
+  //   const data=localStorage.getItem('tiAuth')
+  //   if(data){
+  //       const parseData=JSON.parse(data)
+  //       settiAuth({
+  //           ...tiAuth,
+  //           user:parseData.user,
+  //           token:parseData.token,
+  //           role:parseData.role,
+  //           userData:parseData.userData,
+  //           // userDecryptedId:parseData.user_Id
+  //           userDecryptedId:parseData.decryptedId
+  //       })
         
+  //   }
+  // },[])
+  useEffect(() => {
+    const data = localStorage.getItem('tiAuth');
+    if (data) {
+      const parseData = JSON.parse(data);
+      settiAuth({
+        user: parseData.user || "",
+        token: parseData.token || "",
+        role: parseData.role || "",
+        userData: parseData.userData || null,  // Handle possible missing data
+        userDecryptedId: parseData.userDecryptedId || null,
+        isLoggedIn: parseData.isLoggedIn || false,
+      });
     }
-  },[])
+  }, []);
   return (
     <TIAuthContext.Provider value={[tiAuth, settiAuth]}>
       {children}
