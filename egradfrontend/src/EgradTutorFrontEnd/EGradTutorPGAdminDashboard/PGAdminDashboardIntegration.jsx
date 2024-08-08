b923fe9a3e92be345fa3e93bf6ce39f7cb2f0221
VeenaRagi <163105402+VeenaRagi@users.noreply.github.com>
2024-08-08 12:25:48 +0530

    LeftNAvIntegrated but not showing

--- egradfrontend/src/EgradTutorFrontEnd/EGradTutorPGAdminDashboard/PGAdminDashboardIntegration.jsx
+++ egradfrontend/src/EgradTutorFrontEnd/EGradTutorPGAdminDashboard/PGAdminDashboardIntegration.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import AdminHeader from '../EgradtutorPortalsAdmin/AdminHeader'
+import PGAdminDashBoardInLeftNav from './PGAdminDashBoardInLeftNav'
 
 const PGAdminDashboardIntegration = () => {
     return (
         <>
             <AdminHeader/>
             <div>PGAdminDashboard</div>
-            <p>Need to create left nav</p>
+            {/* <p>Need to create left nav</p> */}
+            <PGAdminDashBoardInLeftNav/>
         </>
     )
 }
