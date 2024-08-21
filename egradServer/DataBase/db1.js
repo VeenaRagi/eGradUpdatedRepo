86a6567ecd80543802970befc1d9df4215aff841
sindhu-121 <sindhu.g@egradtutor.in>
2024-08-21 15:09:23 +0530

    changes

--- egradServer/DataBase/db1.js
+++ egradServer/DataBase/db1.js
@@ -5,7 +5,7 @@ const mysql = require("mysql");
     host: "localhost",
     user: "root",
     password: "",
-    database: "ug_pg_dashboard_database",
+    database: "admin_project",
 
     
   }); 
