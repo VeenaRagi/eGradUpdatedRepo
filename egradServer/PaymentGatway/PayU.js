0f6996e06f3a5bd203c8bad1f6d5e0a938eb3def
sindhu-121 <sindhu.g@egradtutor.in>
2024-08-23 15:52:26 +0530

    changes

--- egradServer/PaymentGatway/PayU.js
+++ egradServer/PaymentGatway/PayU.js
@@ -157,7 +157,7 @@ router.post("/success", async (req, res) => {
  
               <script>
                   document.getElementById('redirectButton').addEventListener('click', function() {
-                      window.location.href = 'http://localhost:3000/Student_dashboard/${encodedUserId}';
+                      window.location.href = 'http://localhost:3000/Student_dashboard/${userIdTesting}/${encryptedBranchId}';
                   });
               </script>
           </body>
